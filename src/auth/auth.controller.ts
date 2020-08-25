f060f41bf06af5168edc90b2f3e46bc0041b1966
BrsJsk <joskicboris@gmail.com>
2020-08-25 20:55:07 +0200

    Signup validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 
@@ -7,8 +7,8 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  signUp(@Body() authCredentials: AuthCredentialsDto) {
-    console.log(authCredentials)
+  signUp(@Body(ValidationPipe) authCredentials: AuthCredentialsDto) {
+    console.log(authCredentials);
     return this.authService.signUp(authCredentials);
   }
 }
