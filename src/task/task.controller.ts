7494f162a548242470733cba465db0763cac4d7e
Boris Joskic <joskicboris95@gmail.com>
2020-12-22 14:05:06 +0100

    Task route auth

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,54 +1,56 @@
 import {
-    Body,
-    Controller,
-    Delete,
-    Get,
-    Param,
-    ParseIntPipe,
-    Patch,
-    Post,
-    Query,
-    UsePipes,
-    ValidationPipe
-} from "@nestjs/common";
-import {TaskService} from "./task.service";
-import {CreateTaskDto} from "./dto/create-task.dto";
-import {TaskStatusValidationPipe} from "./pipes/task-status-validation.pipe";
-import {Task} from "./task.entity";
-import {TaskStatus} from "./task-status.enum";
-import {GetTasksFilterDto} from "./dto/get-tasks-filter.dto";
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
+import { Task } from './task.entity';
+import { TaskStatus } from './task-status.enum';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller("task")
+@Controller('task')
+@UseGuards(AuthGuard())
 export class TaskController {
-    constructor(private taskService: TaskService) {
-    }
+  constructor(private taskService: TaskService) {}
 
-    @Get()
-    findTasks(@Query() filterDto: GetTasksFilterDto) {
-        return this.taskService.findAll(filterDto);
-    }
+  @Get()
+  findTasks(@Query() filterDto: GetTasksFilterDto) {
+    return this.taskService.findAll(filterDto);
+  }
 
-    @Post()
-    @UsePipes(ValidationPipe)
-    create(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
-        return this.taskService.create(createTaskDto);
-    }
+  @Post()
+  @UsePipes(ValidationPipe)
+  create(@Body() createTaskDto: CreateTaskDto): Promise<Task> {
+    return this.taskService.create(createTaskDto);
+  }
 
-    @Get("/:id")
-    findById(@Param("id", ParseIntPipe) id: number): Promise<Task> {
-        return this.taskService.findById(id);
-    }
+  @Get('/:id')
+  findById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+    return this.taskService.findById(id);
+  }
 
-    @Delete("/:id")
-    delete(@Param("id") id: number) {
-        return this.taskService.delete(id);
-    }
+  @Delete('/:id')
+  delete(@Param('id') id: number) {
+    return this.taskService.delete(id);
+  }
 
-    @Patch("/:id/status")
-    updateStatus(
-        @Param("id", ParseIntPipe) id: number,
-        @Body("status", TaskStatusValidationPipe) body: TaskStatus
-    ) {
-        return this.taskService.updateStatus(id, body);
-    }
+  @Patch('/:id/status')
+  updateStatus(
+    @Param('id', ParseIntPipe) id: number,
+    @Body('status', TaskStatusValidationPipe) body: TaskStatus,
+  ) {
+    return this.taskService.updateStatus(id, body);
+  }
 }
